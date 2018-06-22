9ee7295030e3911da87d34f1b2eba6977046964c
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-22 18:08:02 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,6 +2,8 @@ class Transfer
   attr_accessor :sender,:receiver,:amount
   attr_reader :status
   def initialize(sender,receiver,amount)
+    @sender = sender
+    @receiver = receiver
     @status = "pending"
 end
 
